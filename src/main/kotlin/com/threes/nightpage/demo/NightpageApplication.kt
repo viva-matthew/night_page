7b187cc4883db1a9f76c99b48909022c696c9600
viva-matthew <vivainno.matthew@gmail.com>
2022-11-05 04:46:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/threes/nightpage/demo/NightpageApplication.kt
@@ -0,0 +1,11 @@
+package com.threes.nightpage.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NightpageApplication
+
+fun main(args: Array<String>) {
+	runApplication<NightpageApplication>(*args)
+}
---
+++ src/test/kotlin/com/threes/nightpage/demo/NightpageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.threes.nightpage.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NightpageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
